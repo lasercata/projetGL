67153b7e3ea42c7b1654fd372351c58d0e5d2e98
EthanAudoux <ethan@audoux.net>
2025-01-14 16:57:25 +0100

    Add the damage take by the creature
    if somethings enter into the area of the creature, he takes 3hp

--- Entities/Creature/Scripts/testCrea.gd
+++ Entities/Creature/Scripts/testCrea.gd
@@ -16,26 +16,29 @@ func _process(delta: float) -> void:
 	if is_on_floor() == false:
 		velocity += get_gravity() * delta
 	
-		
-	var player = get_parent_node_3d().get_node("Player")
-	var monster_position = global_position
-	var player_position = player.global_position
-		
-	if isPlayerNearby(player) > 1.5 and isPlayerNearby(player) < 15 :
-				
-		var direction := (transform.basis * Vector3(player_position[0] - position.x, 0, player_position[2] - position.z)).normalized()
-		
-		if direction:
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			velocity.z = move_toward(velocity.z, 0, SPEED)
-			
+	if hp <= 0:
+		velocity.y = JUMP_VELOCITY
+	
 	else:
-		velocity.x = 0
-		velocity.z = 0
-		
+		var player = get_parent_node_3d().get_node("Player")
+		var monster_position = global_position
+		var player_position = player.global_position
+			
+		if isPlayerNearby(player) > 1.5 and isPlayerNearby(player) < 15 :
+					
+			var direction := (transform.basis * Vector3(player_position[0] - position.x, 0, player_position[2] - position.z)).normalized()
+			
+			if direction:
+				velocity.x = direction.x * SPEED
+				velocity.z = direction.z * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+				velocity.z = move_toward(velocity.z, 0, SPEED)
+				
+		else:
+			velocity.x = 0
+			velocity.z = 0
+			
 	move_and_slide()
 	
 	
@@ -44,3 +47,9 @@ func isPlayerNearby(player: Node3D) -> int:
 	var player_position = player.global_position
 	var d = player_position.distance_to(monster_position)
 	return d
+	
+
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	hp -= 3 # prends 3 de dégats à chaque fois que qqchose entre dans sa zone
