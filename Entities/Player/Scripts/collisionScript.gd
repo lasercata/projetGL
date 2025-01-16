d9eba6ebafee08bee31e94b33dc3c7f23cd2b2f3
Laszlo A <laszlo.abadie@gmail.com>
2025-01-16 01:59:56 +0100

    Sword implementation

--- Entities/Player/Scripts/collisionScript.gd
+++ Entities/Player/Scripts/collisionScript.gd
@@ -8,6 +8,9 @@ func _on_area_3d_body_entered(body):
 	if (body.name == "Creature"):
 		emit_signal("hit_by_ennemy", body.damage)
 
+func _on_right_hand_button_pressed(name):
+	if name == "by_button":
+		get_tree().reload_current_scene()
 
 #func _on_area_3d_body_exited(body):
 #	if (body.name == "Creature"):
